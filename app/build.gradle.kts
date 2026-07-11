329952a2403346caba619dc320e455faa10f4442
wuxienb <525495055@qq.com>
2026-07-11 08:24:01 +0800

    add gradle wrapper

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,13 +27,14 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
     }
 
-    // 二进制文件太大，不需要压缩，APK 里直接存
     aaptOptions {
-        noCompress("mimocode")
+        noCompress += "mimocode"
     }
 }
 
@@ -41,7 +42,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 }
